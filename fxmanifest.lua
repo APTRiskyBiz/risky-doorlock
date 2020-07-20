b98e7ba540961f5f38d5ca5205def8ef2ac024bd
Success <rjmxtechnology@gmail.com>
2020-07-20 00:28:49 -0500

    add author cfx post and credits

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'bodacious'
 game 'gta5'
 
+description 'risky-doorlock'
+credits 'utkuali fingerprint game -- qalle-fivem esx_doorlock'
+
+
 server_scripts {
 	'config.lua',
 	'server/main.lua'
